8d1821e6045e391990e094dffa61fce333c5c0d8
aracys <aracyscn@gmail.com>
2019-05-31 18:53:27 +0800

    尝试进行状态管理

--- utils/init.js
+++ utils/init.js
@@ -1,5 +1,5 @@
 //为Promise扩展出finally方法
-Promise.prototype.finally = function (callback) {
+Promise.prototype.finally = function(callback) {
     let P = this.constructor;
     return this.then(
         value => P.resolve(callback()).then(() => value),
@@ -9,7 +9,11 @@ Promise.prototype.finally = function (callback) {
     );
 }
 
-Date.prototype.format = function (format) {
+
+/**
+ * 格式化时间
+ */
+Date.prototype.format = function(format) {
     var o = {
         "M+": this.getMonth() + 1,
         "d+": this.getDate(),
@@ -25,13 +29,13 @@ Date.prototype.format = function (format) {
 
 
 var toPromise = fn => {
-    return function (obj = {}) {
+    return function(obj = {}) {
         return new Promise((resolve, reject) => {
-            obj.success = function (res) {
+            obj.success = function(res) {
                 resolve(res)
             }
 
-            obj.fail = function (res) {
+            obj.fail = function(res) {
                 reject(res)
             }
             fn(obj)
@@ -49,86 +53,26 @@ import httpsModule from './https.js'
 //通用配置
 const generalConfig = {
     https: httpsModule.https,
-    api: apiModule,
     toPromise,
     requestCount: 0
 }
 
-const pageConfig = {
-    ...generalConfig,
-    noteLoadState: true,
-    stateHttps: httpsModule.httpState,
-    setResult: (code, data) => {
-        const prevPage = getCurrentPages().length > 1 ? getCurrentPages()[getCurrentPages().length - 2] : undefined
-        if (!prevPage) {
-            return;
-        }
-        if (!prevPage.resultMap) {
-            prevPage.resultMap = new Map();
-        }
-        prevPage.resultMap.set(code, data)
-    },
-    navigateBackForResult: (code, data) => {
-        setResult(code, data)
-        wx.navigateBack()
-    }
-}
 
 const appConfig = {
     ...generalConfig,
     noteLoadState: false,
+    api: apiModule,
 }
 
 
-const originalApp = App;
-App = function (config) {
-    return originalApp(Object.assign(config, appConfig))
-}
+const pageConfig = function(config) {
 
-const originalPage = Page
-Page = function (config) {
-    const {
-        onLoad,
-        onShow,
-        onUnload,
-        arrayData
-    } = config;
-
-    config.onLoad = function (options) {
-        const pages = getCurrentPages();
-        this.prevPage = pages.length > 1 ? pages[pages.length - 2] : undefined
-        if (this.prevPage && this.prevPage.nextParam) {
-            options.pageParam = this.prevPage.nextParam
-            delete pages[pages.length - 2].nextParam
-        }
-        if (typeof onLoad === 'function') {
-            onLoad.call(this, options)
-        }
+    const m = {
+        ...generalConfig,
+        noteLoadState: true,
+        stateHttps: httpsModule.httpState,
     }
 
-    config.onShow = function () {
-        if (typeof onShow === 'function') {
-            onShow.call(this)
-        }
-        if (!this.resultMap || this.resultMap.length == 0 || !this.onPageForResult) {
-            return;
-        }
-        for (var [key, value] of this.resultMap) {
-            this.onPageForResult(key, value)
-        }
-        this.resultMap.clear();
-    }
-
-    config.onUnload = function () {
-        if (this.requestMap && this.requestMap.length > 0) {
-            for (var [key, value] of this.requestMap) {
-                value.abort();
-            }
-        }
-        if (typeof onUnload === 'function') {
-            onUnload.call(this)
-        }
-    }
     /**
      * setData的列表数据，优化一次加载大量数据的问题
      * 
@@ -138,7 +82,7 @@ Page = function (config) {
      * 
      * @return 要添加的数据
      */
-    config.arrayData = function (label, array, handFunc = this.formatItem) {
+    config.arrayData = function(label, array, handFunc = this.formatItem) {
         const data = {};
         if (!array || array.length == 0) {
             return;
@@ -154,5 +98,88 @@ Page = function (config) {
         return data;
     }
 
-    return originalPage(Object.assign(config, pageConfig))
+    /**
+     * 设置结果
+     * 
+     * @param code 结果请求编号
+     * @param data 返回数据
+     */
+    config.setResult = function(code, data) {
+        if (!this.prevPage) {
+            return;
+        }
+        if (!this.prevPage.resultMap) {
+            this.prevPage.resultMap = new Map();
+        }
+        this.prevPage.resultMap.set(code, data)
+    }
+
+    /**
+     * 携带结果返回上一页
+     * 
+     * @param code 结果请求编号
+     * @param data 返回数据
+     */
+    config.navigateBackForResult = function(code, data) {
+        this.setResult(code, data)
+        wx.navigateBack()
+    }
+
+    return Object.assign(m, config)
+}
+
+
+
+const originalApp = App;
+App = function(config) {
+    return originalApp(Object.assign(config, appConfig))
+}
+
+const originalPage = Page
+Page = function(config) {
+    const {
+        onLoad,
+        onShow,
+        onUnload,
+    } = config;
+
+    config.onLoad = function(options) {
+        if (typeof onLoad === 'function') {
+            onLoad.call(this, options)
+        }
+        const pages = getCurrentPages();
+        this.prevPage = pages.length > 1 ? pages[pages.length - 2] : undefined
+        if (this.prevPage && this.prevPage.nextParam) {
+            options.pageParam = this.prevPage.nextParam
+            delete pages[pages.length - 2].nextParam
+        }
+    }
+
+    config.onShow = function() {
+        if (typeof onShow === 'function') {
+            onShow.call(this)
+        }
+        if (!this.resultMap || this.resultMap.length == 0) {
+            return;
+        }
+        if (this.onPageForResult) {
+            for (var [key, value] of this.resultMap) {
+                this.onPageForResult(key, value)
+            }
+        }
+        this.resultMap.clear();
+    }
+
+    config.onUnload = function() {
+        if (typeof onUnload === 'function') {
+            onUnload.call(this)
+        }
+        if (this.requestMap && this.requestMap.length > 0) {
+            for (var [key, value] of this.requestMap) {
+                value.abort();
+            }
+        }
+    }
+
+    return originalPage(pageConfig(config))
 }
